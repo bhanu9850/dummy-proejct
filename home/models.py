c35c127ff941d100eec1eb3edbcd5c8ed1899562
bhanu <kuruvabhanuprakash08@gmail.com>
2024-10-27 22:42:22 +0530

    removed the field phone number from profile model

--- home/models.py
+++ home/models.py
@@ -12,7 +12,7 @@ class Task(models.Model):
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
-    phone_number = models.CharField(max_length=10)
+
 
 
     def __str__(self):
