30dc14fa6baf4db96086baca84ac95876b1b32e5
shiv1119 <shivnandanverma63@gmail.com>
2024-10-28 10:05:45 +0530

    Added Blogs model and registred in admin panel

--- home/models.py
+++ home/models.py
@@ -18,3 +18,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
 
+class Blogs(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="blogs")
+    title = models.CharField(max_length=1000)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
