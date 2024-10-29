2a4b1c9bc624ae3d0a45c57935c8966c4690b642
bhanu <kuruvabhanuprakash08@gmail.com>
2024-10-29 10:47:45 +0530

    deleted the field description in Task model

--- home/models.py
+++ home/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
