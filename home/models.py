26ffca2d6c88420f673ebc7cf8ad812f6ef3e79f
Subhan Jamadar <msubhani.mj@gmail.com>
2024-10-29 12:31:40 +0530

    Added new model with fields

--- home/models.py
+++ home/models.py
@@ -18,3 +18,12 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
 
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=50, choices=[('Pending', 'Pending'), ('In Progress', 'In Progress'), ('Completed', 'Completed')])
+
+    def __str__(self):
+        return self.name
